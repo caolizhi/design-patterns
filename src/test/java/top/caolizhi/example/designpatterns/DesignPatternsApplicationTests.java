a95cf507b2bf2163bf9c44edf843bb6f0ea3810d
cao-lizhi <cao.lizhi@navercorp.com>
2021-04-22 10:51:22 +0800

    init repository

---
+++ src/test/java/top/caolizhi/example/designpatterns/DesignPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package top.caolizhi.example.designpatterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
